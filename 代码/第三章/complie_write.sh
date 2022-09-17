d6a1be4f018ad27669465a7b13b4d02829e5004e
luoyoutao <40891397+luoyoutao@users.noreply.github.com>
2022-09-17 17:31:52 +0800

    Add files via upload

---
+++ 代码/第三章/complie_write.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 这里有两个mbr，因此通过选项来确定编译且写入虚拟硬盘
+read -p "which one mbr.S you want complie and write to hard disk（please input 1 or 2）: " n
+if test ${n} -eq 1
+then
+	nasm -o mbr.bin mbr1.S
+else
+	nasm -I boot.inc -o mbr.bin mbr2.S
+	nasm -I boot.inc -o loader.bin loader.S
+	dd if=loader.bin of=hd60M.img bs=512 count=1 seek=2 conv=notrunc
+fi
+# 下面将mbr.bin写入虚拟硬盘
+dd if=mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
